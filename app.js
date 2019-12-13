62b632ac18f27f314cb28318f7b9191962271cce
Nnamani Ugochukwu Frederick <sirugoh97@gmail.com>
2019-12-13 12:37:50 +0100

    Add new methods to repo

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var main = function () {
     "use strict"
 
     $(".comment-input button").on("click", function (event) {
-        $(".comments").append("<p>This is a new comment</p>")
+        var $new_comment = $("<p>")
+        .text($(".comment-input input").val())
+
+
+        $(".comments").append($new_comment);
     });
 };
 
