bc637092d824c5d8e0e54bbe3d7c794b6c043c54
Nnamani Ugochukwu Frederick <sirugoh97@gmail.com>
2019-12-13 13:27:26 +0100

    Add new changes to the main function to repo

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 var main = function () {
     "use strict"
     var $button = $(".comment-input button");
+    var $input = $(".comment-input input");
+    var $comments = $(".comments");
+    var $new_comment;
 
     $button.on("click", function (event) {
 
-    
-    var $new_comment;
-
-    
-
-    var $input = $(".comment-input input").val();
-
-    var $comments = $(".comments")
-
-    if ($input !== "") {
-        $new_comment = $("<p>").text($input);
+    if ($input.val() !== "") {
+        $new_comment = $("<p>").text($input.val());
 
         $comments.append($new_comment);
+        $input.val("");
+
     }
     })
-        
+
+   
+    $input.on("keypress", function(event) {
+    
+        if (event.keyCode === 13) {
+            if ($input.val() !== "") {
+                $new_comment = $("<p>").text($input.val());
+                $comments.append($new_comment);
+                $input.val("");
+            }
+        }
+    })
 };
 
 $(document).ready(main);
